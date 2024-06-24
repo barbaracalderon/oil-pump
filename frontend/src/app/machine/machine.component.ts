75a44008850ca7276e037dc3d7c1e0db1dfacffe
barbaracalderon <bcalderoni.ti@gmail.com>
2024-06-24 16:34:22 -0300

    feat: create navbar

---
+++ frontend/src/app/machine/machine.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machine',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './machine.component.html',
+  styleUrls: ['./machine.component.css']
+})
+export class MachineComponent { }
